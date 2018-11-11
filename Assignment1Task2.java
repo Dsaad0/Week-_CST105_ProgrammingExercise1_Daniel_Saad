645f4b9086bcf0cf4c6115754158806d3f70ac69
Dsaad0 <44946799+Dsaad0@users.noreply.github.com>
2018-11-11 08:54:04 -0800

    Add files via upload

---
+++ Assignment1Task2.java
@@ -0,0 +1,27 @@
+/* Program:  Assignment 1 Task 2
+ * File:     Assignment1.java
+ * Summary:  Prints Out Name, Favorite Color, and Lottery question
+ * Author:   Daniel Saad
+ * Date:     11/11/2018
+*/
+//Import Scannel Class
+import java.util.Scanner;
+//Declare New Java Class
+public class Assignment1 
+{
+   public static void main(String []args) 
+   {
+      //Initiate Scanner Class
+      Scanner sc = new Scanner(System.in);
+      //Prints Out Name
+      System.out.println("My name is Daniel Saad.");
+      //Prints Out Favorite Color
+      System.out.println("My Favorite color is Blue.");
+      //Prints Out Lottery Question
+      System.out.println("If I won the lottery I would travel!");
+      //Close Scanner
+      sc.close();
+   }
+   
+}
+  
